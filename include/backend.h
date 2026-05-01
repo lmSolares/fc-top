1222a45fa6f4770e190559b3466e0c794e3b0a29
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-01 16:19:34 -0600

    Memory struct and function for the current status of memory

---
+++ include/backend.h
@@ -0,0 +1,16 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+/*
+fc-top backend.h
+*/
+struct Memory{
+    unsigned long MemTotal; // Total usable ram
+    unsigned long MemUsed; // Total memory used
+    unsigned long MemAvailable; // An estimate of how much memory is available for starting new applications, without swapping.
+    double UsagePercentage; // Percentage of memory used
+};
+
+void get_memory(struct Memory *memory);
+
+#endif
