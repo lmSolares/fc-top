a4813734d919a86d5a187013da596095c58ab380
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-01 21:17:50 -0600

    The values ​​in GiB are added to the structure and version of fc-top

--- include/backend.h
+++ include/backend.h
@@ -1,6 +1,8 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
+#define FCTOP_VERSION "0.1.0"
+
 /*
 * fc-top backend.h
 */
@@ -9,6 +11,8 @@ struct Memory{
     unsigned long MemTotal; // Total usable ram
     unsigned long MemUsed; // Total memory used
     unsigned long MemAvailable; // An estimate of how much memory is available for starting new applications, without swapping.
+    double TotalGiB; // Total memory on GiB
+    double UsedGiB; // Total used memory in GiB
     double UsagePercentage; // Percentage of memory used
 };
 
