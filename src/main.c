ca92d22d9832961d7c71adbfda4a1a6a3c2a2a63
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-01 21:26:54 -0600

    Main functions as the controller of the mvc model, adding a memory view.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
-#include "../include/backend.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <ncurses.h>
+#include "backend.h"
+#include "model.h"
 
 int main(){
+
+    struct Memory memory;
+
+    init_ui();
+
+    while(1){
+        get_memory(&memory);
+        render_ui(&memory);
+        int in = getch();
+        if(in == 'q'){
+            break;
+        }
+    }
+
+    end_ui();
+
     return 0;
 }
