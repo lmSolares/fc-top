c8c15cfa1edb8682cf449c7e10c4a41e7f496792
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-06 23:38:03 -0600

    get_memory function

--- include/backend.h
+++ include/backend.h
@@ -7,15 +7,38 @@
 * fc-top backend.h
 */
 
+/**
+* @struct Memory
+* @brief It represents the state of memory.
+*
+* This structure contains the data necessary to represent memory usage.
+*/
 struct Memory{
-    unsigned long MemTotal; // Total usable ram
-    unsigned long MemUsed; // Total memory used
-    unsigned long MemAvailable; // An estimate of how much memory is available for starting new applications, without swapping.
-    double TotalGiB; // Total memory on GiB
-    double UsedGiB; // Total used memory in GiB
-    double UsagePercentage; // Percentage of memory used
+    unsigned long MemTotal; /* Total usable ram. */
+    unsigned long MemUsed; /* Total memory used. */
+    unsigned long MemAvailable; /* An estimate of how much memory is available for starting new applications, without swapping. */
+    double TotalGiB; /* Total memory on GiB. */
+    double UsedGiB; /* Total used memory in GiB. */
+    double UsagePercentage; /* Percentage of memory used. */
 };
 
+/**
+* @brief Function that reads /proc/meminfo to obtain the current state of memory.
+* 
+* @param memory Memory structure in which the data will be stored.
+* 
+* @return void
+*/
 void get_memory(struct Memory *memory);
+
+/**
+ * 
+ */
+struct Uptime{
+    long seconds;
+    int minutes;
+    int hours;
+    int days;
+};
 
 #endif
