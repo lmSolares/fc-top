e3f10e5b447129206f8f59abad5e162f9492539e
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-26 20:43:10 -0600

    Add methods for rendering processes and confirming when to kill a process.

--- include/model.h
+++ include/model.h
@@ -23,10 +23,12 @@ void init_ui();
 void end_ui();
 
 // Renders
-void render_dashboard(const struct Memory *memory, const struct Uptime *uptime, const struct Swap *swap);
+void render_dashboard(const struct Memory *memory, const struct Uptime *uptime, const struct Swap *swap, const struct ProcessList *plist, int selected, int *scroll);
 void render_memory(WINDOW *win, const struct Memory *memory);
 void render_uptime(WINDOW *win, const struct Uptime *uptime);
 void render_swap(WINDOW *win, const struct Swap *swap);
 void render_bar(WINDOW *win, double percentage, int bar_width, int y, int x);
+void render_processes(WINDOW *win, const struct ProcessList *plist, int selected, int *scroll);
+int confirm_kill(int pid, const char *name);
 
 #endif
